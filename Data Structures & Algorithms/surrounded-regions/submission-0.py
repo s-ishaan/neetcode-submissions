1c6ecae3bc753d1333c525a1c5b1705959ee7074
Ishaan Singh <83677927+s-ishaan@users.noreply.github.com>
2026-08-29 05:38:22 -0400

    Add: surrounded-regions - submission-0

---
+++ Data Structures & Algorithms/surrounded-regions/submission-0.py
@@ -0,0 +1,45 @@
+from collections import deque
+
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        rows = len(board)
+        columns = len(board[0])
+        q = deque()
+        safe = set()
+
+
+        for i in range(rows):
+            for j in range(columns):
+                if i ==0 or i==rows-1 or j ==0 or j==columns-1:
+                    if board[i][j] == 'O':
+                        q.append((i,j))
+                        safe.add((i,j))
+        directions = [
+            (1,0),
+            (0,1),
+            (-1,0),
+            (0,-1)
+        ]
+
+        
+        while q:
+            x,y = q.popleft()
+
+            for dx, dy in directions:
+                nx = x + dx
+                ny = y +dy
+
+                if (nx <0 or ny<0
+                    or nx>=rows or ny>=columns
+                    or board[nx][ny] == 'X' 
+                    or (nx,ny) in safe):
+                    continue
+                
+                q.append((nx,ny))
+                safe.add((nx,ny))
+
+        for i in range(rows):
+            for j in range(columns):
+                if (i,j) not in safe:
+                    board[i][j] = 'X'
+    
