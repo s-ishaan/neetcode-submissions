7e3a36a95b7ff47b80accd2d4ca741051ea025bd
Ishaan Singh <83677927+s-ishaan@users.noreply.github.com>
2026-09-07 14:01:11 -0400

    Add: coin-change-ii - submission-1

---
+++ Data Structures & Algorithms/coin-change-ii/submission-1.py
@@ -0,0 +1,27 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        memo = {}
+        n = len(coins)
+        res = []
+
+        dp = {} #(i, amount):#
+        def dfs(i, amount):
+            if amount == 0:
+                return 1
+
+            if (i, amount) in dp:
+                return dp[(i,amount)]
+
+            if i == n or amount<0:
+                return 0
+
+            skip = dfs(i+1, amount)
+            take = dfs(i, amount-coins[i])
+
+            dp[(i,amount)] = take + skip
+            
+            return dp[(i,amount)]
+
+
+    
+        return dfs(0, amount)
